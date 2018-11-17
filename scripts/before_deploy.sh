586bb52950aa90156112936ed6cb4d38c11ac323
Bruno Tavares <connect+github@bltavares.com>
2018-11-17 23:19:59 +0000

    Try to compile a single artifact

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,47 +1,46 @@
 # This script takes care of building the crate and packaging it for release.
 
-PKG_NAME="hypercore"
+PKG_NAME="rust-over-jna-example"
 
 set -ex
 
 main() {
-  local src=$(pwd) \
-        stage= \
-        linking_args=
+  local src=$(pwd) stage
 
   case $TRAVIS_OS_NAME in
-    linux)
-      stage=$(mktemp -d)
-      ;;
-    osx)
-      stage=$(mktemp -d -t tmp)
-      ;;
+  linux)
+    stage=$(mktemp -d)
+    ;;
+  osx)
+    stage=$(mktemp -d -t tmp)
+    ;;
   esac
 
   test -f Cargo.lock || cargo generate-lockfile
 
-  # TODO: combine with -C lto
+  cargo install cargo-crate-type
+
   case $TYPE in
-      static)
-          linking_args="--crate-type staticlib"
-          ;;
-      *)
-          linking_args="--crate-type cdylib"
-          ;;
+  static)
+    cargo crate-type static
+    ;;
+  *)
+    cargo crate-type dynamic
+    ;;
   esac
 
-  cross rustc --lib --target $TARGET --release -- $linking_args
+  cross rustc --lib --target $TARGET --release -- -C lto
 
   case $TYPE-$TRAVIS_OS_NAME in
-      static-*)
-          cp target/$TARGET/release/lib$PKG_NAME.a $stage/
-          ;;
-      *-osx)
-          cp target/$TARGET/release/lib$PKG_NAME.dylib $stage/
-          ;;
-      *)
-          cp target/$TARGET/release/lib$PKG_NAME.so $stage/
-          ;;
+  static-*)
+    cp target/$TARGET/release/lib$PKG_NAME.a $stage/
+    ;;
+  *-osx)
+    cp target/$TARGET/release/lib$PKG_NAME.dylib $stage/
+    ;;
+  *)
+    cp target/$TARGET/release/lib$PKG_NAME.so $stage/
+    ;;
   esac
 
   cd $stage
